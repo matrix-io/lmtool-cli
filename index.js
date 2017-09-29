f16b79593215018b6b6e48931059a7b696eb7737
Dilan Nery <dnerylopes@gmail.com>
2017-09-29 12:40:01 -0300

    add cli support

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-const fs = require('fs');
-const lmtool = require('./src/lmtool');
-
-const fileStream = fs.createReadStream(__dirname + '/dict.txt');
-lmtool.run(fileStream).then(link => console.log(link)).catch(err => console.log(err));
+module.exports = require('./src/lmtool');
--- src/request-with-redirect.js
+++ src/request-with-redirect.js
@@ -1,15 +1,14 @@
 const request = require('request');
 const url = require('url');
+require('colors');
 
 function requestMultipartForm(method, pageUrl, formData) {
 	return new Promise((resolve, reject) => {
-		console.log("Requesting: ", pageUrl);
-		
+		console.log("Requesting: ".green, pageUrl);
 		request({method, uri:pageUrl, formData}, function(err, res, body) {
 			if (err) { reject(err); }
-
 			if (res.statusCode > 300 && res.statusCode < 400 && res.headers.location) {
-				console.log("Redirecting...");
+				console.log("Redirecting...".blue);
 				redirectUrl = res.headers.location + "/";
 
 		        requestMultipartForm("GET", redirectUrl)
